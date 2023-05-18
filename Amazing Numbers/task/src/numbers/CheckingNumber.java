e1dc71aba64a72c41e6576a66f4f465459f39d3f
Piotr Gabryś <pgabrys@outlook.com>
2023-05-18 20:56:10 +0200

    Teoretycznie, mam logikę odnośnie sprawdzania liczby pod kątem dwóch warunków:          - są 3 metody, o którym mowa pożniej          - udało się nawet wykorzystać wczesniejszy kod (CheckingNumber)  bez żadnej modyfikacji          - liczby, który pasują są dodawane do tablicy
             * No to teraz trzeba ogarnąć drukowaniego tego wszystkiego na tablicę

--- Amazing Numbers/task/src/numbers/CheckingNumber.java
+++ Amazing Numbers/task/src/numbers/CheckingNumber.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class CheckingNumber {
 
-    private static boolean isOdd(Number number) {
+    protected static boolean isOdd(Number number) {
         return number.getChosenNumber() % 2 != 0;
     }
 
-    private static boolean isEven(Number number) {
+    protected static boolean isEven(Number number) {
         return number.getChosenNumber() % 2 == 0;
     }
 
-    private static boolean isBuzz(Number number) {
+    protected static boolean isBuzz(Number number) {
         boolean buzz = false;
         long firstPart = number.getChosenNumber() / 10;
         long secondPart = number.getChosenNumber() % 10;
@@ -25,11 +25,11 @@ public class CheckingNumber {
         return buzz;
     }
 
-    private static boolean isDuck(Number number) {
+    protected static boolean isDuck(Number number) {
         return String.valueOf(number.getChosenNumber()).contains("0");
     }
 
-    private static boolean isPalindromic(Number number) {
+    protected static boolean isPalindromic(Number number) {
         boolean palindromic = false;
         String numberToString = String.valueOf(number.getChosenNumber());
         StringBuffer reversedNumber = new StringBuffer(numberToString);
@@ -42,7 +42,7 @@ public class CheckingNumber {
         return palindromic;
     }
 
-    private static boolean isGapful(Number number) {
+    protected static boolean isGapful(Number number) {
         boolean gapful = false;
         String stringNumber = Long.toString(number.getChosenNumber());
         String[] parts = stringNumber.split("");
@@ -59,7 +59,7 @@ public class CheckingNumber {
         return gapful;
     }
 
-    private static boolean isSpy(Number number) {
+    protected static boolean isSpy(Number number) {
         boolean spy = false;
         long sum;
         long product = 1;
@@ -81,12 +81,12 @@ public class CheckingNumber {
         return spy;
     }
 
-    private static boolean isSunny(Number number) {
+    protected static boolean isSunny(Number number) {
         double sqrt = Math.sqrt(number.getChosenNumber() + 1);
         return sqrt % 1 == 0;
     }
 
-    private static boolean isSquare(Number number) {
+    protected static boolean isSquare(Number number) {
         double sqrt = Math.sqrt(number.getChosenNumber());
         return sqrt % 1 == 0;
     }
--- Amazing Numbers/task/src/numbers/CheckingNumbersWithTwoProperties.java
+++ Amazing Numbers/task/src/numbers/CheckingNumbersWithTwoProperties.java
@@ -1,13 +1,61 @@
 package numbers;
 
+import java.util.Arrays;
+
 public class CheckingNumbersWithTwoProperties {
 
     static long[] selectingTwoProperties(Number number, Property firstProperty, Property secondProperty) {
         int counter = 0;
         long numberUnderCheck = number.getChosenNumber();
         long repeat = number.getRepeats();
-        long[] firstSelection = new long[(int) repeat];
+        long[] selectedNumbers = new long[(int) repeat];
+
+        do {
+            if (checkingFirstProperty(number,firstProperty) == true
+                    && checkingSecondProperty(number, secondProperty) == true) {
+                selectedNumbers[counter] = numberUnderCheck;
+                numberUnderCheck++;
+                number.setChosenNumber(numberUnderCheck);
+                counter++;
+            }
+        } while (counter == repeat);
+
+
         /**
+         *----------------18.05.2023---------------------
+         * Teoretycznie, mam logikę odnośnie sprawdzania liczby pod kątem dwóch warunków:
+         *  - są 3 metody, o którym mowa pożniej
+         *  - udało się nawet wykorzystać wczesniejszy kod (CheckingNumber) bez żadnej modyfikacji
+         *  - liczby, który pasują są dodawane do tablicy
+         *
+         * No to teraz trzeba ogarnąć drukowaniego tego wszystkiego na tablicę
+         * ---------------16.05.2023---------------------
+         * No niestety, wychodzi na to, że koncept z for i reużyciem kodu nie zadziała...
+         * Obecny koncept to pętla do-while, warunek wpisany.
+         *
+         * Trzeba napisać trzy metody:
+         *  1. Metoda która będzie sprawdzać liczbę - tutaj cała logika do EVEN itd.,
+         *     oparta na swicthu i ENUM,zwraca ona boolean
+         *  2. Prosta metoda, która bierze liczbę i enuma, przekazuje ją do pierwszej metody a następnie
+         *     dostaje zwrot boolean, który ustawia "firstPropertyTrue".
+         *  3. Robi to samo co metoda 2, tylko dla "secondPropertyTrue
+         *
+         * Następnie If, który sprawdza czy booleany z metody 2 i 3 są prawdziwe, a jeśli tak,
+         * dodaje sprawdzaną liczbę do tablicy i podbija counter.
+         *
+         * -------------- 15.05.2023-----------------------
+         * Użyć pętli for, liczba startowa to input, licza powtórzeń pętli również.
+         * Następnie, już w samej pętli użyć powrówniania właściwości z inputu do ENUM'a i za pomocą tego
+         * odpalić odpowiednią metodę - tutaj myślę, aby wykorzystać metody ze sprawdzania pojedyńczej liczby -
+         *  zwraca ona boolean'a
+         *
+         *  Następnie, za pomocą if'a sprawdzić czy są dwa "true" - jeśli tak, dodać do tablicy/listy
+         *  i podbić counter.
+         *
+         *  Następnie pozostaje obróbka listy aby już wydrukować do konsoli
+         *  -------------------------------------------------------
+         *
+         *
          * Tutaj MUSZĘ wykorzystać kod do sprawdzania liczby z jedną właściowością (bo po co pisać jeszcze
          * raz to samo?) - czyli wywołać metodę z tej klasy. Tylko tutaj może mi pomysł rozjebać fakt,
          * że w tamtej metodzie pobiera ilość powtórzeń.... Ale może by to spróbować obejść poprzez próbę
@@ -19,7 +67,49 @@ public class CheckingNumbersWithTwoProperties {
          * właściwości), a jak nie to, to nie.
          */
 
-        return firstSelection;
+        return selectedNumbers;
+    }
+
+    private static boolean checkingNumber(Number number, Property property) {
+        boolean propertyOk;
+        switch (property) {
+            case EVEN -> {
+               propertyOk = CheckingNumber.isEven(number);
+            }
+            case ODD -> {
+                propertyOk = CheckingNumber.isOdd(number);
+            }
+            case BUZZ -> {
+                propertyOk = CheckingNumber.isBuzz(number);
+            }
+            case DUCK -> {
+                propertyOk = CheckingNumber.isDuck(number);
+            }
+            case PALINDROMIC -> {
+                propertyOk = CheckingNumber.isPalindromic(number);
+            }
+            case GAPFUL -> {
+                propertyOk = CheckingNumber.isGapful(number);
+            }
+            case SPY -> {
+                propertyOk = CheckingNumber.isSpy(number);
+            }
+            case SQUARE -> {
+                propertyOk = CheckingNumber.isSquare(number);
+            }
+            case SUNNY -> {
+                propertyOk = CheckingNumber.isSunny(number);
+            }
+        }
+        return propertyOk;
+    }
+
+    private static boolean checkingFirstProperty(Number number, Property property) {
+        return checkingNumber(number, number.getFirstProperty());
+    }
+
+    private static boolean checkingSecondProperty(Number number, Property property) {
+        return checkingNumber(number, number.getSecondProperty());
     }
 
 }
